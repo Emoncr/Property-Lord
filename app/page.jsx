7e01ac7f98adbc343ff9d035281be4807293762b
Emoncr <biplobemon75945@gmail.com>
2024-10-04 09:06:53 +0600

    fix: Fixed hero section pages

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,8 @@ default                                      function Home() {
     
     return (
         <main>
+                
+                   
                     <HomePage />
            
         
