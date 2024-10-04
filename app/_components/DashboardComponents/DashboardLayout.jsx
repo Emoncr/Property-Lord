1b5a1c4e2f2025702a63f33965684b7d3c52ed50
Emoncr <biplobemon75945@gmail.com>
2024-10-04 09:14:33 +0600

    fix: Fixed dashboard formating issue

--- app/_components/DashboardComponents/DashboardLayout.jsx
+++ app/_components/DashboardComponents/DashboardLayout.jsx
@@ -6,7 +6,7 @@ import ToggleMenuDashboard from './common/ToggleMenuDashboard';
 
 const DashboardLayout = ({ children }) => {
     return (
-        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="custom_class grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
             <div className="hidden border-r bg-muted/40 md:block">
                 <div className="flex h-full max-h-screen flex-col gap-2">
                     <div className="flex h-14 items-center border-b border-border px-4 lg:h-[60px] lg:px-6">
